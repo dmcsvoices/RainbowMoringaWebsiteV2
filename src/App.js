a463662c3cbea37f7f93b5b6dac081b5661b0ebf
Darren Santos <65102731+dmcsvoices@users.noreply.github.com>
2022-03-06 15:46:14 -0800

    got the nav column and fontawesome working

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee, faLeaf, faCannabis, faSeedling } from "@fortawesome/free-solid-svg-icons";
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,16 +10,29 @@ import ArticleTitle from './components/ArticleTitle';
 import ArticleText_whereToBuy from './components/ArticleText_whereToBuy';
 import ArticleText_NeighborhoodGrower from './components/ArticleText_NeighborhoodGrower';
 import ArticleText_moringaComparison from './components/ArticleText_moringaComparison';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(fab, faCheckSquare, faCoffee,  faLeaf, faCannabis, faSeedling);
 
 function App() {
   
   return (
+    <div className="mainBox">
 
-    <div className="App">
+      
       <Header />
-        <ArticleText_whereToBuy />
-        <ArticleText_NeighborhoodGrower />
-        <ArticleText_moringaComparison />
+      <div className="articleBox">
+      <div className="vertNav">
+        <FontAwesomeIcon className="navIcon" icon="fa-solid fa-seedling" size="3x" />
+        <FontAwesomeIcon icon="fa-solid fa-leaf" size="3x"/></div>
+      <div className="App">
+          
+            <ArticleText_whereToBuy />
+            <ArticleText_NeighborhoodGrower />
+            <ArticleText_moringaComparison />
+          
+        </div>
+        
+      </div>
       <Footer />
     </div>
 
